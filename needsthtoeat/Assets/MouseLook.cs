50140c2bea7cca7ad20e630da5f118e66d3e944b
Tran Nguyen Sinh <69780117+svenriksen@users.noreply.github.com>
2021-08-04 22:07:21 +0700

    oke

--- needsthtoeat/Assets/MouseLook.cs
+++ needsthtoeat/Assets/MouseLook.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class MouseLook : MonoBehaviour
 {
-    public float mouseSens = 300f;
+    public float mouseSens = 100f;
 
     public Transform playerBody;
 
@@ -14,13 +14,16 @@ public class MouseLook : MonoBehaviour
     void Start()
     {
         Cursor.lockState = CursorLockMode.Locked;
+        //Cursor.visible = false;
+        transform.rotation = Quaternion.Euler(0, 0, 0);
+
     }
 
     // Update is called once per frame
     void Update()
     {
         float mouseX = Input.GetAxis("Mouse X") * mouseSens * Time.deltaTime;
-        float mouseY = Input.GetAxis("Mouse Y") * mouseSens * Time.deltaTime;
+        float mouseY = Input.GetAxis("Mouse Y") * mouseSens * Time.deltaTime * 3f;
 
         xRotation -= mouseY;
 
