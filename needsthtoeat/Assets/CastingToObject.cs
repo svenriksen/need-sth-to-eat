c732459897dd717933c5a35eb9eee855cfa12c6e
Tran Nguyen Sinh <69780117+svenriksen@users.noreply.github.com>
2021-08-18 20:35:24 +0700

    much things

--- needsthtoeat/Assets/CastingToObject.cs
+++ needsthtoeat/Assets/CastingToObject.cs
@@ -12,7 +12,11 @@ public class CastingToObject : MonoBehaviour
     private Animator anim;
 
     private bool drawerOpened = false;
+    private bool drawer1Opened = false;
+    private bool drawer2Opened = false;
     private bool doorOpened = false;
+    private bool fridgeUpOpened = false;
+    private bool fridgeDownOpened = false;
 
 
     public float maxDistance = 3;
@@ -71,6 +75,53 @@ public class CastingToObject : MonoBehaviour
             anim.SetBool("doorOpened", !doorOpened);
         }
 
+        if (theObject.transform.tag == "Drawer1")
+        {
 
+            //This line will get the Animator from  Parent of the door that was hit by the raycast.
+            anim = theObject.transform.GetComponentInParent<Animator>();
+
+            //This will set the bool the opposite of what it is.
+            drawer1Opened = !drawer1Opened;
+
+            //This line will set the bool true so it will play the animation.
+            anim.SetBool("drawer1Opened", !drawer1Opened);
+        }
+        if (theObject.transform.tag == "Drawer2")
+        {
+
+            //This line will get the Animator from  Parent of the door that was hit by the raycast.
+            anim = theObject.transform.GetComponentInParent<Animator>();
+
+            //This will set the bool the opposite of what it is.
+            drawer2Opened = !drawer2Opened;
+
+            //This line will set the bool true so it will play the animation.
+            anim.SetBool("drawer2Opened", !drawer2Opened);
+        }
+        if (theObject.transform.tag == "FridgeUp")
+        {
+
+            //This line will get the Animator from  Parent of the door that was hit by the raycast.
+            anim = theObject.transform.GetComponentInParent<Animator>();
+
+            //This will set the bool the opposite of what it is.
+            fridgeUpOpened = !fridgeUpOpened;
+
+            //This line will set the bool true so it will play the animation.
+            anim.SetBool("fridgeUpOpened", !fridgeUpOpened);
+        }
+        if (theObject.transform.tag == "FridgeDown")
+        {
+
+            //This line will get the Animator from  Parent of the door that was hit by the raycast.
+            anim = theObject.transform.GetComponentInParent<Animator>();
+
+            //This will set the bool the opposite of what it is.
+            fridgeDownOpened = !fridgeDownOpened;
+
+            //This line will set the bool true so it will play the animation.
+            anim.SetBool("fridgeDownOpened", !fridgeDownOpened);
+        }
     }
 }
